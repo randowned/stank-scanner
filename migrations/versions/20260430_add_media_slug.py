b6d02a2e9f1b74bea32739160c72389fd3de45d7
Randowned <kaanyurukcu+randowned@gmail.com>
2026-05-01 00:25:52 +0200

    v2.33.1

--- migrations/versions/20260430_add_media_slug.py
+++ migrations/versions/20260430_add_media_slug.py
@@ -8,14 +8,14 @@ Create Date: 2026-04-30 14:00:00.000000
 from __future__ import annotations
 
 from alembic import op
-from sqlalchemy import String, text
+import sqlalchemy as sa
 
 revision = "add_media_slug"
 down_revision = "add_media_tables"
 
 
 def upgrade() -> None:
-    op.add_column("media_items", op.Column("slug", String(64), nullable=True))
+    op.add_column("media_items", sa.Column("slug", sa.String(64), nullable=True))
     op.create_unique_constraint("uq_media_slug", "media_items", ["guild_id", "slug"])
 
 
